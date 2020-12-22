f2255d23173682a7abc782fe412a857aef1dbccc
nano <76194982+nano2nano@users.noreply.github.com>
2020-12-22 20:29:40 +0900

    Make the argument about the turn of send_result2line not required

--- send_result2line.py
+++ send_result2line.py
@@ -3,11 +3,12 @@ import os
 import requests
 
 
-def send_result2line(win, lose, drow, max_turn, min_turn, avg_turn):
+def send_result2line(win, lose, drow, max_turn=None, min_turn=None, avg_turn=None):
     message = '\nwin: {}\nlose: {}\ndrow: {}\n\n勝率: {}%'.format(
         win, lose, drow, int(win/(win+lose+drow)*100))
-    message += '\n最大ターン数: {}\n最小ターン数: {}\n平均ターン数: {}'.format(
-        max_turn, min_turn, avg_turn)
+    if (str(max_turn)+str(min_turn)+str(avg_turn)).isdecimal():
+        message += '\n最大ターン数: {}\n最小ターン数: {}\n平均ターン数: {}'.format(
+            max_turn, min_turn, avg_turn)
     token = os.environ['LINE_API_TOKEN']
     api_url = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': f'Bearer {token}'}
